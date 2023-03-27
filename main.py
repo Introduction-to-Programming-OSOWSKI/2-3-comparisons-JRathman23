da9fcda58166b0f21f18209de26af0af33120f19
JRathman23 <831350@mystma.org>
2023-03-27 14:41:59 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if (x>y):
+        return("true")
+    else:
+        return("false")
+greaterThan(1,2)
+
+def lessThan(x,y):
+    if(x<y):
+        return("true")
+    else:
+        return("false")
+lessThan(4,1)
+
+def equalTo(x,y):
+    if(x==y):
+        return("true")
+    else:
+        return("false")
+equalTo(1,1)
+
+def greaterOrEqual(x,y):
+    if(x>=y):
+        return("true")
+    else:
+        return("false")
+greaterOrEqual(5,2)
+
+def lessOrEqual(x,y):
+    if(x<=y):
+        return("true")
+    else:
+        return("false")
+lessOrEqual(1,6)
